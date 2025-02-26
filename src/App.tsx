2636ee659a53e06c1f6d9612d19d687fd35c2cbb
leticia.almeida <leticia.almeida@solution4fleet.com.br>
2025-02-26 19:17:37 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box textAlign="center" p={5}>
+      <Text fontSize="2xl" fontWeight="bold" color="teal.500">
+       Iniciando projeto Charada Diversões
+      </Text>
+    </Box>
+  );
+}
+
+export default App;
